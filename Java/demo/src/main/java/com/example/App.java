d6c8be23d6fd197466e1830d5eff0c42b29edffe
Luarce <118055883+Luarce@users.noreply.github.com>
2024-12-17 15:48:14 +0000

    2024年12月18日 0:48

--- Java/demo/src/main/java/com/example/App.java
+++ Java/demo/src/main/java/com/example/App.java
@@ -7,6 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        System.out.println("Hhgj");
+        System.out.println("にほんごをかいてみる～～～");
     }
 }
